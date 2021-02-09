b71dc8ffc18f36dbdb4d8cf868e56d1cfa0c5a96
Min <min.sun@efrei.net>
2021-02-09 17:51:27 +0100

    Commit Exercise 21

--- Flickrapp/app/src/main/java/com/example/flickrapp/MyAdapter.java
+++ Flickrapp/app/src/main/java/com/example/flickrapp/MyAdapter.java
@@ -5,24 +5,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.Vector;
+
 public class MyAdapter extends BaseAdapter {
+    private Vector<String> vector = new Vector<String>();
+
     @Override
     public int getCount() {
-        return 0;
+        return vector.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return vector.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return vector.indexOf(position);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         return null;
+    }
+
+    public void dd(String url) {
+        // Add url to the vector
+        vector.add(url);
     }
 }
